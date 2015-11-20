6c95251e1ad95835d042b742eda8a6fa311ca654
kamrenz <kamren@developintelligence.com>
2015-11-20 11:40:29 -0700

    Simplifying the server

--- server.js
+++ server.js
@@ -1,41 +1,29 @@
-// Importing express module for our node middleware
+// Importing express module for our Node middleware
 var express = require('express');
-
+//Import file I/O module
 var fs = require('fs');
 
-// ------------------------------------------------------------------
-// Configurable variables section...
-// ------------------------------------------------------------------
-// Web server port
-// If 8080 doesn't work try 9080
-var BASE_PORT = process.env.port || 8080;
-
-// Compute the working directory for serving static files
-// makes assumptions about layout of node and directory structure
-// working directories/projects etd.
-var ROOT_DIR = __dirname + '/';
-ROOT_DIR = fs.realpathSync(ROOT_DIR);
-if (!fs.existsSync(ROOT_DIR)) {
-	console.log('Error: cannot find working directory: ' + ROOT_DIR);
-	exit();
-}
-
-/**
- * Create the "express" server for routing and static files.
- **/
+//Express application
 var app = express();
 
-/**
- * Adds a simple logging, "mounted" on the root path.
- * Using Express middleware
- **/
+// Web server port
+// BTW: If 8080 doesn't work try 9080
+var BASE_PORT = process.env.port || 8080;
+
+// Static file root directory & check
+var ROOT_DIR = __dirname + '/';
+ROOT_DIR = fs.realpathSync(ROOT_DIR);
+
+//Add application route logging
 app.use(function(req, res, next) {
 	console.log('%s %s', req.method, req.url);
 	next();
 });
 
+//Serve static folder
 app.use(express.static(ROOT_DIR));
 
+//Log the server variables
 app.listen(BASE_PORT, function() {
 	console.log('Node server started @ http://localhost:' + BASE_PORT);
 	console.log('Serving static files from ' + ROOT_DIR);
