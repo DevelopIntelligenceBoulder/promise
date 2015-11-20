80c3f264f98d34b962d9a5789699d095af10a345
kamrenz <kamren@developintelligence.com>
2015-11-20 13:53:17 -0700

    Adding ES6 promise architecture

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ A look a different promise architecture implementation
 ## jQuery Promise
 `/promise-jquery` gives the jQuery architecture for implementing promises.
 
+## ES6 Promise
+`/promise-es6` gives the ES6 architecture for implementing promises.
+
 ## JavaScript Training
 Would you like to dig into this more. [DevelopIntelligence](http://www.developintelligence.com) can help. Take a look at our [Advanced JavaScript](http://www.developintelligence.com/catalog/web-development-training/core-javascript/advanced-javascript) to get your team up to speed!
 
---
+++ promise-es6/src/location.js
@@ -0,0 +1,77 @@
+//Namespace declaration
+(function() {
+    //DevelopIntelligence namespace
+    window.DI = window.DI || {};
+})();
+
+/**
+ * Geolocation Module that retrieves the user coordinates.
+ *  Attached to the DevelopIntelligence namespace.
+ **/
+DI.location = (function() {
+ 	'use strict';
+
+	//Possible error codes thrown via the Geolocation API
+	var ERROR_TYPE_CODES = [
+		'Unknown error',
+		'Permission denied by user',
+		'Position is not available',
+		'Request timed out'
+	];
+
+	/**
+	 * Gets the location from the browser.
+	 * @return Promise promise that is fulfilled when the Geolocation has been found
+	 **/
+	var getLocation = function getLocation() {
+
+        //ES6 Promise object
+		return new Promise(function(resolve, reject) {
+
+			//Geolocation API
+			navigator.geolocation.getCurrentPosition(
+                /**
+                 * Successful geolocation interaction.
+                 * @param position an object containing coordinates of the location
+                 **/
+                function resolveLocation(position) {
+
+                    //Coordinates object
+                    var coordinates = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    };
+
+                    //Resolve the promise making the interaction successful
+                    resolve(coordinates);
+
+                },
+                /**
+                 * Failed Geolocation information.
+                 * @param error an object containing a code and additional message information property
+                 **/
+                function resolveError(error) {
+
+                    //Error message to log
+                    var errorMessage = ERROR_TYPE_CODES[error.code];
+
+                    //Error codes 0 and 2 have extra message information wrapped into the error message
+                    if (error.code === 0 || error.code === 2) {
+                        errorMessage += ' ' + error.message;
+                    }
+
+                    //Reject the promise making the interaction a failure
+                    reject('Geolocation error: ' + errorMessage);
+
+            });
+
+		});
+
+	};
+
+    //Location module API
+ 	return {
+ 		getLocation: getLocation
+ 	};
+
+ })();
