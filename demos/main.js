975e6b3de77b7bf68d24f863c6f116347994014e
kamrenz <kamren@developintelligence.com>
2015-11-25 11:25:43 -0700

    Modifying main.js

--- demos/main.js
+++ demos/main.js
@@ -1,2 +1,10 @@
-//Call external module
-DI.location.getLocation();
+(function() {
+    'use stict';
+
+    //DevelopIntelligence namespace
+    window.DI = window.DI || {};
+
+    //Call external module
+    DI.location.getLocation();
+
+})();
